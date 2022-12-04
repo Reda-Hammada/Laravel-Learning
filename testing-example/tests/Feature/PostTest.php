586e681cd8ce85d6f1b40b33901c92a2c058dc83
Reda Hammada <hammada.reda.dev@gmail.com>
2022-12-04 18:00:40 +0100

    example testing added

---
+++ testing-example/tests/Feature/PostTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class PostTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_example()
+    {
+        $response = $this->get('/posts');
+
+        $response->assertStatus(200);
+        $response->assertSeeText('POSTS:');
+    }
+}
